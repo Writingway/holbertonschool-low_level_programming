79e02b721c7870ac16c7797d14a4d95920aed03b
Writingway <111703108+Writingway@users.noreply.github.com>
2025-10-20 09:05:14 +0000

    fix: fix errors & warning from betty style code

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
-int main(void) {
-    char holberton[] = "\"Programming is like building a multilingual puzzle";
-    puts(holberton);
-    return 0;
+/**
+ * main - Entry point
+ * Description: 'Output must be with puts'
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+char holberton[] = "\"Programming is like building a multilingual puzzle";
+puts(holberton);
+return (0);
 }
