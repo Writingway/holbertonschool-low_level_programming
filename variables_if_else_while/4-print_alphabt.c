d19f42e6f0a5ad263bfd19d3720ce481f09d5e88
Writingway <111703108+Writingway@users.noreply.github.com>
2025-10-21 13:52:34 +0000

    fix: using for instead of while, and remove q,e from the alphabet

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: 'Conditions'
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet == 'q' || alphabet 'e')
+		{
+			continue;
+		}
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
