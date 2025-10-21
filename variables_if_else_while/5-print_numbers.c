bbcc44f9139e476ce11ba1a8e040341db67a4eed
Writingway <111703108+Writingway@users.noreply.github.com>
2025-10-21 13:58:57 +0000

    feat: create script that print number from 0 to 9 in base 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: 'Conditions'
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+	putchar('\n');
+	return (0);
+}
