0805ad879aab8b65db03281311561774218d1047
Writingway <111703108+Writingway@users.noreply.github.com>
2025-10-22 09:18:05 +0000

    fix: remove trailing whitespace from file

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,9 @@
 #include "main.h"
 /**
- * main - check the code
- *
+ * print_alphabet - check the code
  * Return: Always 0.
-
  * print_alphabet(void) - entry point
- *
  * Description: This function prints the alphabet, in lowercase
- *
- * Return: void
- 
 */
 void print_alphabet(void)
 {
