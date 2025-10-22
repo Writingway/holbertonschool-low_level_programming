af4dfdb62925a980a09102219b5d60d292be9b2c
Writingway <111703108+Writingway@users.noreply.github.com>
2025-10-22 12:54:29 +0000

    feat: create function that computes the absolute value of an integer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - check if is lower
+ * @n: The character to check
+ * Return: Always 0.
+ * Description: This function prints the alphabet, in lowercase 10 time
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = (-1) * n;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
