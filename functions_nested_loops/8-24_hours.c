367cd846bf52b87c1b7cec23da838acfa65a0117
Writingway <111703108+Writingway@users.noreply.github.com>
2025-10-23 15:12:13 +0000

    fix: using putchar instead of printf & remake the logic for calculate in ASCII table

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,12 +9,25 @@ void jack_bauer(void)
 {
 	int number_min = 59;
 	int number_hours = 23;
+	int hour_tens;
+	int hour_units;
+	int min_tens;
+	int min_units;
 
 	for (number_hours = 0; number_hours < 24; number_hours++)
 	{
+		hour_tens = number_hours / 10;
+		hour_units = number_hours % 10;
 		for (number_min = 0; number_min < 60; number_min++)
 		{
-			printf("%02d:%02d\n", number_hours, number_min);
+			min_tens = number_min / 10;
+			min_units = number_min % 10;
+			_putchar('0' + hour_tens);
+			_putchar('0' + hour_units);
+			_putchar(':');
+			_putchar('0' + min_tens);
+			_putchar('0' + min_units);
+			_putchar('\n');
 		}
 	}
 }
