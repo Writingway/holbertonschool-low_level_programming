7037953c964d92035c0c99849dd6e272497abcd3
Writingway <111703108+Writingway@users.noreply.github.com>
2025-10-24 09:27:26 +0000

    feat: create function that adds two integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - check if is lower
+ *@a: The character to check
+ *@b: The character to check
+ * Return: Always 0.
+ * Description: This function prints the alphabet, in lowercase 10 time
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
