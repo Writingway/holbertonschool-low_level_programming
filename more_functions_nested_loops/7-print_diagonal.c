9fd9d08fac1a13a37c819c2edb8547daf087a2d2
Writingway <111703108+Writingway@users.noreply.github.com>
2025-10-27 15:33:16 +0000

    fix: remake the condition of the function

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,16 +10,18 @@ void print_diagonal(int n)
 
 	int i, j = 0;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
 	for (i = 0; i < n; i++)
 	{
-	if (n <= 0)
-		_putchar('\n');
-	for (j = 0; j < i; j++)
-	{
-		_putchar(' ');
-	}
-	_putchar(' ');
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
 	_putchar('\\');
 	_putchar('\n');
 	}
