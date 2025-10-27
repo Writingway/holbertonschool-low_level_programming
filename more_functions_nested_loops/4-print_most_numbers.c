793456c42f720e4fd0175baa6db3eb949b272d63
Writingway <111703108+Writingway@users.noreply.github.com>
2025-10-27 09:54:46 +0000

    feat: write a function that print number without 2 & 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+		int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num == 2 || num == 2)
+		{
+			continue;
+		}
+		_putchar('0' + num);
+	}
+
+	_putchar('\n');
+}
