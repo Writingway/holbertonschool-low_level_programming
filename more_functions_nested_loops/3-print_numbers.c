3f75faeb8a95b2f6d5163fe69f218f176d81fe64
Writingway <111703108+Writingway@users.noreply.github.com>
2025-10-27 09:27:12 +0000

    feat: write a function that print the numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		_putchar('0' + num);
+
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,8 +4,10 @@
  * prototype - prototype of all my function
 */
 
+int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif
