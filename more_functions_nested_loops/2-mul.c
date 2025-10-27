8e48b59ff782ca4e93b6bca4258ed3ec186ad285
Writingway <111703108+Writingway@users.noreply.github.com>
2025-10-27 09:19:24 +0000

    feat: write a function that multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplication of a & b param
+ * @a: param to check
+ * @b: param to check
+ * Return: Always 0.
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
