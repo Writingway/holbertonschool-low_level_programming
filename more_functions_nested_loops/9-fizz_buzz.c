cd35e8013753efefdc62aac91e86510783513616
Writingway <111703108+Writingway@users.noreply.github.com>
2025-10-28 08:25:20 +0000

    feat: "The Fizz-Buzz Test" write a program that print 1 to 100 with Fizz Buzz & FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i = 100;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && 5 != 0)
+			printf(" Fizz");
+		else if (i % 5 == 0 && 3 != 0)
+			printf(" Buzz");
+		else if (i % 5 == 0 && i % 3 == 0)
+			printf(" FizzBuzz");
+		else
+			printf(" %d", i);
+	}
+
+
+	return (0);
+}
