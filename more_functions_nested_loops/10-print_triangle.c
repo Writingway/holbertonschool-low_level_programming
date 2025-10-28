bc1d405e4d81044064d177017a15e7296a76f828
Writingway <111703108+Writingway@users.noreply.github.com>
2025-10-28 08:46:56 +0000

    feat: write a function that print a triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - check the code
+ * @size: int to check
+ * Retursize: Always 0.
+ */
+void print_triangle(int size)
+{
+	int i, j = 0;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = i; j < size; j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 0; j <= i; j++)
+		{
+			_putchar('#');
+		}
+	_putchar('\n');
+	}
+}
