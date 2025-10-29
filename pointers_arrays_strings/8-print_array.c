a3189d71f42a020b274ec986b77b8f153097e09e
Writingway <111703108+Writingway@users.noreply.github.com>
2025-10-29 10:46:39 +0000

    feat: write a function that prints n elements of an array of int

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,12 +8,14 @@
  */
 void print_array(int *a, int n)
 {
+	int i;
 
-	for (n = 0; n < a; n++)
+	for (i = 0; i < n; i++)
 	{
-		if (n == 0)
-			printf("%d", n);
+		if (i == 0)
+			printf("%d", a[i]);
 		else
-			printf(", %d", n);
+			printf(", %d", a[i]);
 	}
+	printf("\n");
 }
