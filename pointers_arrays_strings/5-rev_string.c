fd107fa034b8702028435db72973ffd76fa84fe0
Writingway <111703108+Writingway@users.noreply.github.com>
2025-10-29 08:57:33 +0000

    feat write a function that reverses a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string - print in reverse
+ *@s: pointer to string
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	/*
+	*Pour s[count] j'ai le nombre de caratere
+	*	avoir mon premier et derniere carateres
+	*	Diviser ma longueur par 2
+	*	Puis swap dans tmp
+	*/
+
+	int count = 0;
+	int i;
+	int len;
+	int temp;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	len = count - 1;
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len];
+		s[len--] = temp;
+	}
+}
