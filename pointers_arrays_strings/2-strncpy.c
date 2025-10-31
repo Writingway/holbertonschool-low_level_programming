74500f553aaac901235ffd13474a844b4502fe33
Writingway <sendmeat4time@gmail.com>
2025-10-31 09:36:46 +0100

    feat: write a function that copies a string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - concatenates two string
+ *@dest: destination
+ *@src: source
+ *@n: n bytes
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[i] = src[i];
+
+
+	dest[i] = '\0';
+
+	return (dest);
+}
