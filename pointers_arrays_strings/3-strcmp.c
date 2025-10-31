00e3c1b58924c3bea4b72c1d9f9a019ec9226dff
Writingway <sendmeat4time@gmail.com>
2025-10-31 11:01:18 +0100

    fix: change conditions of the loop

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcmp - concatenates two string
+ * _strcmp - compare two string
  *@s1: destination
  *@s2: source
  * Return: Always 0.
@@ -9,8 +9,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 == *s2 + 1)
-		if (*s1 + 1 == 0)
-			return (0);
+	while (*s1 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
 	return (*s1 - *s2);
 }
