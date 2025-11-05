58e007a7ca7863c7569fd1edc4b45178d92791b5
Writingway <sendmeat4time@gmail.com>
2025-11-05 16:06:55 +0100

    fix: betty is not friendly with me

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -6,7 +6,6 @@
  * @i: int args
  * Return: On success 1.
  **/
-
 int _sqrt_recursion_helper(int n, int i)
 {
 	if (i * i > n)
@@ -19,8 +18,8 @@ int _sqrt_recursion_helper(int n, int i)
 /**
  * _sqrt_recursion - print a string
  * @n: int args
- * Return: On success 1. **/
-
+ * Return: On success 1. 
+**/
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
