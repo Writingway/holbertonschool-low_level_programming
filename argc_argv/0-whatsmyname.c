5033d209af82b9a7b00fc5b3e9787f18007fc9a2
Writingway <sendmeat4time@gmail.com>
2025-11-07 14:57:50 +0100

    feat: write a function that prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: counter of the args
+ * @argv: array of the args
+ * Return: Always 0 (Success)
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
