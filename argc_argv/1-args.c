3e045ba92bae4d1e1eb5fcf7e6124cd72f6c87eb
Writingway <sendmeat4time@gmail.com>
2025-11-07 15:00:44 +0100

    feat: write a function that prints the number of args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: counter of the args
+ * @argv: array of the args
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
