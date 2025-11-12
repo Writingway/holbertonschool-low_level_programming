7220ce231f41aca79f8756d987c5ea93935289e4
Writingway <sendmeat4time@gmail.com>
2025-11-12 14:50:50 +0100

    fix: changing by not using calloc and make it hand made

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,13 +9,36 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *arr = calloc(nmemb, size);
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int k = 0;
+	int **grid;
 
 	if (size == 0)
 		return (NULL);
 
-	if (arr == NULL)
+	grid = malloc(nmemb * sizeof(int *));
+
+	if (grid == NULL)
 		return (NULL);
 
-	return (arr);
+	for (i = 0; i < size; i++)
+	{
+		grid[i] = malloc(nmemb * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (k = 0; k < i; k++)
+			{
+				free(grid[k]);
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < size; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+
+	return (grid);
 }
