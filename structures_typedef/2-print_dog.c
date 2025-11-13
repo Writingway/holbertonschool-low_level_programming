4b746dbf9d38a3a41af8421ee6cbb45b4bb09c72
Writingway <sendmeat4time@gmail.com>
2025-11-13 14:17:43 +0100

    fix: remove return

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,7 +15,6 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 	{
 		printf("Name: (nil)\n");
-		return;
 	}
 	else
 		printf("Name: %s\n", d->name);
@@ -23,14 +22,12 @@ void print_dog(struct dog *d)
 	if (d->age <= 0)
 	{
 		printf("Age: (nil)\n");
-		return;
 	}
 	else
 		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 	{
 		printf("Owner: (nil)\n");
-		return;
 	}
 	else
 		printf("Owner: %s\n", d->owner);
