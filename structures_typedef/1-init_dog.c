b11f8032db18bc8d81753b3137eed6368c26afde
Writingway <sendmeat4time@gmail.com>
2025-11-13 13:49:45 +0100

    fix: adding condition to the function

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - check the allocation
@@ -12,6 +13,9 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
