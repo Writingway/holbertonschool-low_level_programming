f0d4a68d2592ecdc3d7e0874fab2071cbd795787
Writingway <sendmeat4time@gmail.com>
2025-11-13 15:46:46 +0100

    fix: i made all the code with no function and only by hand

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,14 +12,45 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i = 0, j = 0;
+	char *newOwner = malloc(sizeof(char) * (j + 1));
+	char *newName = malloc(sizeof(char) * (i + 1));
+
 	dog_t *tmp = malloc(sizeof(dog_t));
 
 	if (tmp == NULL)
 		return (NULL);
 
-	tmp->name = name;
+	if (newName == NULL)
+	{
+		free(tmp);
+		return (NULL);
+	}
+
+	if (newOwner == NULL)
+	{
+		free(newName);
+		free(tmp);
+		return (NULL);
+	}
+
+	while (name[i] != '\0')
+		i++;
+
+	while (owner[j] != '\0')
+		j++;
+
+	for (i = 0; name[i] != '\0'; i++)
+		newName[i] = name[i];
+	newName[i] = '\0';
+
+	for (j = 0; owner[j] != '\0'; j++)
+		newOwner[j] = owner[j];
+	newOwner[j] = '\0';
+
+	tmp->name = newName;
+	tmp->owner = newOwner;
 	tmp->age = age;
-	tmp->owner = owner;
 
 	return (tmp);
 }
