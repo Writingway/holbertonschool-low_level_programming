8a6d02af938edfd6306eea8bda6a2ac4cb6c9bda
Writingway <sendmeat4time@gmail.com>
2025-11-13 14:13:07 +0100

    feat: write a function that prints a struct dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * print_dog - check the allocation
+ * @d: pointer
+ * Return: Nothing.
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+		return;
+	}
+	else
+		printf("Name: %s\n", d->name);
+
+	if (d->age <= 0)
+	{
+		printf("Age: (nil)\n");
+		return;
+	}
+	else
+		printf("Age: %.1f\n", d->age);
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+		return;
+	}
+	else
+		printf("Owner: %s\n", d->name);
+
+}
