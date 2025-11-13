4581f2c34f1f6341d2b754d061fe37ecaf205637
Writingway <sendmeat4time@gmail.com>
2025-11-13 13:06:48 +0100

    feat: define a new type struct

---
+++ structures_typedef/README.md
@@ -0,0 +1,3 @@
+# holbertonschool-low_level_programming
+
+structure_typedef
---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - check the code
+ *@name : name of the dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ * Return: Always 0.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
