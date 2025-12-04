d39bc9aca046717c3280625ff5c985616470c8ea
Writingway <sendmeat4time@gmail.com>
2025-12-04 14:39:51 +0100

    feat: implement dlistint_len function to count nodes in a doubly linked list

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - prints all the elements of a dlistint_t list
+ * @h: pointer to the head of the list
+ * Return: the number of nodes
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
