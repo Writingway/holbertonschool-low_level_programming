56dc1bf2d0a3f7ffd2ad92412cfd3da4f47c815a
Writingway <sendmeat4time@gmail.com>
2025-12-05 14:25:23 +0100

    feat: add file I/O functions and main program to read and print text files

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <fcntl.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to the name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print, 0 if it fails
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, rd, wr;
+	char *buffer;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	rd = read(fd, buffer, letters);
+	if (rd == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	wr = write(STDOUT_FILENO, buffer, rd);
+	if (wr == -1 || wr != rd)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+	return (wr);
+}
