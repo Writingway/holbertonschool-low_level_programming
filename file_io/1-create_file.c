fd7251042d45f2823581682e4949798c984e704f
Writingway <sendmeat4time@gmail.com>
2025-12-05 14:28:08 +0100

    feat: implement create_file function to create a file and write text to it

---
+++ file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <fcntl.h>
+
+/**
+ * create_file - creates a file
+ * @filename: pointer to the name of the file to create
+ * @text_content: pointer to a string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr;
+	size_t len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
+		wr = write(fd, text_content, len);
+		if (wr == -1 || (size_t)wr != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
